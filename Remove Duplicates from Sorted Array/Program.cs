81d3664caf2b9828f0342eefdd6a44c75e1d8d20
George Darchiashvili <Gdarchiashvili@hotmail.com>
2023-09-10 16:31:14 +0400

    resolved

--- Remove Duplicates from Sorted Array/Program.cs
+++ Remove Duplicates from Sorted Array/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!list.Contains(nums[i]))
+                {
+                    list.Add(nums[i]);
+                    nums[list.Count - 1] = nums[i];
+                }
+            }
+
+            foreach (int i in nums)
+                Console.Write(i + ", ");
+
+            return list.Count;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] nums = new int[] { 1, 1, 1, 2, 2, 3, 3, 4 };
+            RemoveDuplicates(nums);
+
+
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHello, World!");
         }
     }
 }
