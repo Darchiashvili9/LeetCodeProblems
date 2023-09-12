b451428ea2e784f70329ca88aa3865923a71f077
George Darchiashvili <Gdarchiashvili@hotmail.com>
2023-09-12 18:14:34 +0400

    Rotate_Array

---
+++ Rotate Array/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Rotate_Array
+{
+    internal class Program
+    {
+        public static void Rotate(int[] nums, int k)
+        {
+
+
+
+
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 1, 1, 2, 2, 3, 3, 4 };
+
+            Rotate(nums, 5);
+
+
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHello, World!");
+        }
+    }
+}
